517873328180b3e455154017e78995b67f1e2530
Alexander01998 <contact.wurstimperium@gmail.com>
2015-05-06 22:14:34 +0200

    Add RegexListEntry.target
    Allows regexes that are only applied to certain files.

--- Wurst Enigma/src/tk/wurst_client/enigma/regexlist/RegexListEntry.java
+++ Wurst Enigma/src/tk/wurst_client/enigma/regexlist/RegexListEntry.java
@@ -7,19 +7,39 @@
  */
 package tk.wurst_client.enigma.regexlist;
 
+import java.util.HashSet;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import com.google.common.collect.Sets;
+
 public class RegexListEntry
 {
+	private final HashSet<String> target;
 	private final Pattern regex;
 	private final String replacement;
 	
 	public RegexListEntry(String regex, String replacement)
 		throws PatternSyntaxException
 	{
+		this(null, regex, replacement);
+	}
+	
+	public RegexListEntry(String[] target, String regex, String replacement)
+		throws PatternSyntaxException
+	{
+		this.target = target == null ? null : Sets.newHashSet(target);
 		this.regex = Pattern.compile(regex);
 		this.replacement = replacement;
+	}
+	
+	public boolean isTarget(String name)
+	{
+		if(target == null)
+			return true;
+		if(target.contains(name))
+			return true;
+		return false;
 	}
 	
 	public String replaceAll(String content) throws IllegalArgumentException
